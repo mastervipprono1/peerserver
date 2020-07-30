b92aa7f52d8c550acbf68555c30ec1cb2cabf380
phong  nguyen <phongit1995@gmail.com>
2020-07-30 14:20:18 +0700

    fix ssl

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: process.env.PORT ||3000 ,path:"/"});
+console.log(path.join(__dirname,"/mydomain.key"));
+const peerServer = PeerServer({ 
+    port: process.env.PORT ||3000 ,
+    ssl: {
+        key: fs.readFileSync(path.join(__dirname,"/mydomain.key")),
+        cert: fs.readFileSync(path.join(__dirname,"/mydomain.crt")),
+    },
+    path:"/"});
